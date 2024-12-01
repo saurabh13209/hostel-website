3e9c7b8b284a39fc8dbcb20550b86006f35a6720
saurabh13209 <agrawal99saurabh@gmail.com>
2024-12-01 17:21:52 +0530

    basic code

--- src/App.js
+++ src/App.js
@@ -1,34 +1,7 @@
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import React from 'react';
 
 const App = () => {
   const callLambdaFunction = async () => {
-    // const url = 'https://hbbga5xqeyxbma6n6rxtxzu44q0kejif.lambda-url.eu-north-1.on.aws/';
     const url = "https://lxtustrzzroeasqsrkl7ffruka0iubfj.lambda-url.us-east-2.on.aws/dev";
     try {
       const response = await fetch(url);
